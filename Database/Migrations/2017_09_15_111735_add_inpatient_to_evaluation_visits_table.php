a77c2f99f2f85fa58ac4f928aa32e80e9405a577
BravoGidi <bkiptoo@collabmed.com>
2017-09-19 10:25:03 +0300

    migration error

--- Database/Migrations/2017_09_15_111735_add_inpatient_to_evaluation_visits_table.php
+++ Database/Migrations/2017_09_15_111735_add_inpatient_to_evaluation_visits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddInpatientToEvaluationVisitsTable_ extends Migration
+class AddInpatientToEvaluationVisitsTable extends Migration
 {
     /**
      * Run the migrations.
