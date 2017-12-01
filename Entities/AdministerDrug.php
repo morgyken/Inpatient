39dd9c6824fe7a59cae4a9a7100c25e123825105
kisiara <kisiarafrancis@gmail.com>
2017-12-01 12:34:22 +0000

    adding changes on the administering of drugs - revamp

--- Entities/AdministerDrug.php
+++ Entities/AdministerDrug.php
@@ -5,21 +5,6 @@ namespace Ignite\Inpatient\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Ignite\Evaluation\Entities\Patients;
 
-/**
- * Ignite\Inpatient\Entities\AdministerDrug
- *
- * @property int $id
- * @property int $prescription_id
- * @property int $administered
- * @property \Carbon\Carbon|null $created_at
- * @property \Carbon\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\AdministerDrug whereAdministered($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\AdministerDrug whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\AdministerDrug whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\AdministerDrug wherePrescriptionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\AdministerDrug whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class AdministerDrug extends Model
 {
     protected $fillable = [
