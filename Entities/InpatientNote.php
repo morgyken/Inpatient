2ddfc91fe9214f23cda86daa2f2a069436959d04
Samuel Dervis <samueldervis@gmail.com>
2017-12-08 11:48:27 +0300

    Model documentation

--- Entities/InpatientNote.php
+++ Entities/InpatientNote.php
@@ -4,6 +4,25 @@ namespace Ignite\Inpatient\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Ignite\Inpatient\Entities\InpatientNote
+ *
+ * @property int $id
+ * @property int $visit_id
+ * @property string $title
+ * @property string $type
+ * @property string $notes
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereNotes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\InpatientNote whereVisitId($value)
+ * @mixin \Eloquent
+ */
 class InpatientNote extends Model
 {
     protected $fillable = [
--- Entities/Rebate.php
+++ Entities/Rebate.php
@@ -5,6 +5,24 @@ namespace Ignite\Inpatient\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Ignite\Settings\Entities\Schemes;
 
+/**
+ * Ignite\Inpatient\Entities\Rebate
+ *
+ * @property int $id
+ * @property int $scheme_id
+ * @property float $amount
+ * @property string $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Ignite\Settings\Entities\Schemes $scheme
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereSchemeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Inpatient\Entities\Rebate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Rebate extends Model
 {
     protected $fillable = [
