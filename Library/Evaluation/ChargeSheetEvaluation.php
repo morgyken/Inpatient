8a9848948d0e8065c68aae021e025a9b1cdf7d9b
Samuel Dervis <samueldervis@gmail.com>
2017-12-08 11:46:46 +0300

    Bootstrap models

--- Library/Evaluation/ChargeSheetEvaluation.php
+++ Library/Evaluation/ChargeSheetEvaluation.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Ignite\Inpatient\Library\Evaluation;
 
+use Ignite\Evaluation\Entities\Investigations;
 use Ignite\Evaluation\Entities\Visit;
 use Ignite\Inpatient\Entities\ChargeSheet;
 use Ignite\Inpatient\Library\Interfaces\EvaluationInterface;
@@ -9,8 +11,8 @@ use Carbon\Carbon;
 
 class ChargeSheetEvaluation implements EvaluationInterface
 {
-    protected $visit; 
-    
+    protected $visit;
+
     /*
     * Initialize the visit property
     */
@@ -48,7 +50,7 @@ class ChargeSheetEvaluation implements EvaluationInterface
 
         $ward = $this->visit->admission->ward;
 
-        $days = $charges->filter(function($charge){
+        $days = $charges->filter(function ($charge) {
 
             return $charge->ward_id;
 
@@ -70,16 +72,15 @@ class ChargeSheetEvaluation implements EvaluationInterface
     {
         $charges = $this->visit->chargeSheet->load('charge');
 
-        $charges = $charges->filter(function($charge){
-            
+        $charges = $charges->filter(function ($charge) {
+
             return $charge->charge_id;
 
         });
 
-        foreach($charges as $charge)
-        {
+        foreach ($charges as $charge) {
             $charge->name = $charge->charge->name;
-        }   
+        }
 
         return $charges;
     }
@@ -91,43 +92,43 @@ class ChargeSheetEvaluation implements EvaluationInterface
     {
         $charges = $this->visit->chargeSheet->load('consumable');
 
-        $consumableCharges = $charges->filter(function($charge){
-            
+        $consumableCharges = $charges->filter(function ($charge) {
+
             return $charge->consumable_id;
 
         });
 
-        $charges = $consumableCharges->map(function($charge){
+        $charges = $consumableCharges->map(function ($charge) {
 
             $consumable = $charge->consumable;
 
             $product = $consumable->product;
-            
+
             return [
-                'name' => $product->name, 
-                'units' => $consumable->quantity, 
-                'cost' => $consumable->price, 
+                'name' => $product->name,
+                'units' => $consumable->quantity,
+                'cost' => $consumable->price,
                 'price' => $consumable->amount,
-                'used_on' => Carbon::parse($consumable->created_at)->toDayDateTimeString(),  
+                'used_on' => Carbon::parse($consumable->created_at)->toDayDateTimeString(),
             ];
-        
+
         });
 
-        $charges = $charges->groupBy('name')->map(function($charge, $key){
-            
+        $charges = $charges->groupBy('name')->map(function ($charge, $key) {
+
             $units = $charge->pluck('units')->sum();
             $cost = $charge->pluck('cost')->first();
 
             return [
                 'name' => $charge->pluck('name')->first(),
-                'units' => $units, 
-                'cost' =>  $cost, 
-                'price' => $units * $cost, 
+                'units' => $units,
+                'cost' => $cost,
+                'price' => $units * $cost,
             ];
         });
 
         $charges['total'] = $charges->pluck('price')->sum();
-        
+
         return $charges;
     }
 
@@ -138,13 +139,13 @@ class ChargeSheetEvaluation implements EvaluationInterface
     {
         $charges = $this->visit->chargeSheet->load('dispense');
 
-        $prescriptionCharges = $charges->filter(function($charge){
-            
+        $prescriptionCharges = $charges->filter(function ($charge) {
+
             return $charge->dispensing_id;
 
         });
 
-        $charges = $prescriptionCharges->map(function($charge){
+        $charges = $prescriptionCharges->map(function ($charge) {
 
             $dispense = $charge->dispense;
 
@@ -157,25 +158,25 @@ class ChargeSheetEvaluation implements EvaluationInterface
             $cost = $this->visit->patients->schemes ? $drug->insurance_p : $drug->cash_p;
 
             return [
-                'name' => $drug->name, 
-                'units' => $details->quantity, 
-                'cost' => $cost, 
+                'name' => $drug->name,
+                'units' => $details->quantity,
+                'cost' => $cost,
                 'price' => $details->price,
-                'dispensed' => Carbon::parse($dispense->created_at)->toDayDateTimeString(),  
+                'dispensed' => Carbon::parse($dispense->created_at)->toDayDateTimeString(),
             ];
 
         });
 
-        $charges = $charges->groupBy('name')->map(function($charge, $key){
+        $charges = $charges->groupBy('name')->map(function ($charge, $key) {
 
             $units = $charge->pluck('units')->sum();
             $cost = $charge->pluck('cost')->first();
 
             return [
                 'name' => $charge->pluck('name')->first(),
-                'units' => $units, 
-                'cost' =>  $cost, 
-                'price' => $units * $cost, 
+                'units' => $units,
+                'cost' => $cost,
+                'price' => $units * $cost,
             ];
         });
 
@@ -188,14 +189,13 @@ class ChargeSheetEvaluation implements EvaluationInterface
     * Get Investigations
     */
     public function investigations()
-    {   
+    {
         $charges = $this->visit->chargeSheet->load('investigation');
 
-        $charges = $charges->filter(function($charge){
+        $charges = $charges->filter(function ($charge) {
 
-            return ($charge->investigation_id && strpos($charge->investigation->type, 'inpatient') !== false) ;
+            return ($charge->investigation_id && strpos($charge->investigation->type, 'inpatient') !== false);
         });
-
         $diagnostics = $this->getInvestigations($charges, 'diagnostics');
 
         $laboratory = $this->getInvestigations($charges, 'laboratory');
@@ -211,12 +211,12 @@ class ChargeSheetEvaluation implements EvaluationInterface
     * GetProcedures
     */
     public function procedures()
-    {   
+    {
         $charges = $this->visit->chargeSheet->load('investigation');
 
-        $charges = $charges->filter(function($charge){
+        $charges = $charges->filter(function ($charge) {
 
-            return ($charge->investigation_id && strpos($charge->investigation->type, 'inpatient') !== false) ;
+            return ($charge->investigation_id && strpos($charge->investigation->type, 'inpatient') !== false);
         });
 
         $doctor = $this->getInvestigations($charges, 'treatment');
@@ -231,37 +231,35 @@ class ChargeSheetEvaluation implements EvaluationInterface
 
     public function getInvestigations($charges, $type)
     {
-        $charges = $charges->filter(function($charge) use($type){
-            
-            return (strpos($charge->investigation->type, $type) !== false) ;
+        $charges = $charges->filter(function ($charge) use ($type) {
+
+            return (strpos($charge->investigation->type, $type) !== false);
 
         });
-
-        $charges = $charges->map(function($charge){
-
+        $charges = $charges->map(function ($charge) {
+            /** @var Investigations $investigation */
             $investigation = $charge->investigation;
 
             $procedure = $investigation->procedures;
-            
             return [
-                'name' => $procedure->name, 
-                'units' => $investigation->quantity, 
-                'cost' => $investigation->price, 
-                'price' => $investigation->amount, 
+                'name' => $procedure->name,
+                'units' => $investigation->quantity,
+                'cost' => $investigation->price,
+                'price' => $investigation->amount,
+                'paid' => true
             ];
 
         });
-
-        $charges = $charges->groupBy('name')->map(function($charge, $key){
+        $charges = $charges->groupBy('name')->map(function ($charge, $key) {
 
             $units = $charge->pluck('units')->sum();
             $cost = $charge->pluck('cost')->first();
 
             return [
                 'name' => $charge->pluck('name')->first(),
-                'units' => $units, 
-                'cost' =>  $cost, 
-                'price' => $units * $cost, 
+                'units' => $units,
+                'cost' => $cost,
+                'price' => $units * $cost,
             ];
         });
 
